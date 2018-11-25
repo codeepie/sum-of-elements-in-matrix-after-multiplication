6eab7f3c3672259e0c5d255b170839859f12849a
yogendra singh <yogendra20799@gmail.com>
2018-11-25 10:00:12 +0530

    Initial Commit

---
+++ sum of matrix mult/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sum of matrix mult
+//
+//  Created by yogendra singh on 11/25/18.
+//  Copyright © 2018 yadavboy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
